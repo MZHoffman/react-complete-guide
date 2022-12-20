0a5471741b1621951019a8ce94dda76a8c202a96
Michal Hoffman <michal.z.hoffman@gmail.com>
2022-12-20 16:46:11 +0000

    Expence date module

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div>
-      <div className='expense-item'>Date</div>
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>Title</h2>
-        <div className='expense-item__price'>Amount</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   )
 }
+
 export default ExpenseItem
