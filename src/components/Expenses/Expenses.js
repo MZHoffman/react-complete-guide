f2f089579345683bfe9564b12e8fc188ba2cdef9
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-01-03 18:04:14 +0000

    Enabling filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,15 +17,20 @@ const Expenses = (props) => {
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={dropdownYear} onSelectedYear={selectedYear} />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {props.items
+        .map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+        .filter(
+          (expense) =>
+            expense.props.date.getFullYear().toString() === dropdownYear
+        )}
     </Card>
   )
 }
-
 export default Expenses
