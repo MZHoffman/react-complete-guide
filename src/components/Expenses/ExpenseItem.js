0a072e9d1fcd2fecdece55e314541122b8e0e5bd
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-01-03 14:15:11 +0000

    changing ExpenseItem into stateless component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title)
-  const clickHandler = () => {
-    setTitle('Updated!')
-    console.log(title)
-  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -17,7 +12,6 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
